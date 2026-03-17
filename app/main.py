dde79031ae28ded5ef55eca90aea3be8f17a1103
Nnazarka <nazarkulyk6@gmail.com>
2026-03-17 09:35:04 +0100

    Implement main function to log current timestamp to a file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        ts = datetime.now()
+        filename = f"app-{ts.hour}_{ts.minute}_{ts.second}.log"
+        content = ts.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(content)
+
+        print(f"{content} {filename}")
+
+        from time import sleep
+
+        sleep(1)
 
 
 if __name__ == "__main__":
